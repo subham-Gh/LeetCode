759cf2fca6d51ebec92d7ee7aa87ebf8fef6db1f
Subham Ghosh <155185819+subham-Gh@users.noreply.github.com>
2024-08-26 23:21:24 +0530

    Time: 57 ms (40.02%), Space: 49.5 MB (96.04%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let result = [];
+    
+    function backtrack(current, open, close) {
+        if (current.length === 2 * n) {
+            result.push(current);
+            return;
+        }
+        
+        if (open < n) {
+            backtrack(current + '(', open + 1, close);
+        }
+        
+        if (close < open) {
+            backtrack(current + ')', open, close + 1);
+        }
+    }
+    
+    backtrack('', 0, 0);
+    return result;
+};
