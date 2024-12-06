19a00b4be1e866dc01151f85456cf48f5db8883d
Subham Ghosh <155185819+subham-Gh@users.noreply.github.com>
2024-12-06 22:52:08 +0530

    Time: 51 ms (68.42%), Space: 65.7 MB (20.00%) - LeetHub

---
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} banned
+ * @param {number} n
+ * @param {number} maxSum
+ * @return {number}
+ */
+var maxCount = function(banned, n, maxSum) {
+    let bannedSet = new Set(banned);
+    let total = 0, count = 0;
+    for (let i = 1; i <= n; i++) {
+        if (!bannedSet.has(i)) {
+            total += i;
+            if (total <= maxSum) count++;
+            else break;
+        }
+    }
+    return count;
+};
