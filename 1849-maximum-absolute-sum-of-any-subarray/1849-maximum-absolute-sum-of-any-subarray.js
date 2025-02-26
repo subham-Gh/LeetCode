1d0eb2d3cf36eac73f534116a0542124c3e3a4e6
Subham Ghosh <155185819+subham-Gh@users.noreply.github.com>
2025-02-26 22:59:23 +0530

    Time: 7 ms (12.2%), Space: 67.3 MB (7.32%) - LeetHub

---
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxAbsoluteSum = (nums, s = 0) => {
+    nums = nums.map((v, i) => s = s + v);
+    return Math.max(...nums, 0) - Math.min(...nums, 0);
+};
