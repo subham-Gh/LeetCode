c7887557cd4575fc7ae93764e2745159510d6cfd
Subham Ghosh <155185819+subham-Gh@users.noreply.github.com>
2025-08-13 23:12:33 +0530

    Time: 5 ms (84.23%), Space: 65.3 MB (45.73%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+function isPowerOfThree(n) {
+    if (n < 1) return false;
+    
+    while (n % 3 === 0) {
+        n /= 3;
+    }
+    
+    return n === 1;
+}
