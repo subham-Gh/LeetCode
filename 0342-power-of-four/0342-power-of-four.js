1434768afcd41103f337e50d7f0a2747c906e1ab
Subham Ghosh <155185819+subham-Gh@users.noreply.github.com>
2025-08-16 01:53:21 +0530

    Time: 0 ms (100%), Space: 57.4 MB (27.95%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if (n < 1) return false;
+
+    while (n % 4 === 0) {
+        n = n / 4;
+    }
+
+    return n === 1;
+};
